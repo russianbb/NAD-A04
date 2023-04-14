59d916975d0e91403e1aa1d12ef864887b111d6f
Bruno Borges Russian <bruno.russian.i@gmail.com>
2023-04-14 13:40:01 -0400

    Posts: add views, templates and JS

---
+++ src/posts/static/posts/main.js
@@ -0,0 +1,198 @@
+const postsBox = document.getElementById("posts-box")
+const spinnerBox = document.getElementById("spinner-box")
+const loadBtn = document.getElementById("load-btn")
+const endBox = document.getElementById("end-box")
+
+const postForm = document.getElementById("post-form")
+const title = document.getElementById("id_title")
+const body = document.getElementById("id_body")
+const csrf = document.getElementsByName("csrfmiddlewaretoken")
+
+const url = window.location.href
+
+const alertBox = document.getElementById("alert-box")
+
+const dropzone = document.getElementById('my-dropzone')
+const addBtn = document.getElementById('add-btn')
+const closeBtns = [...document.getElementsByClassName('add-modal-close')]
+
+const getCookie = (name) => {
+    let cookieValue = null;
+    if (document.cookie && document.cookie !== '') {
+        const cookies = document.cookie.split(';');
+        for (let i = 0; i < cookies.length; i++) {
+            const cookie = cookies[i].trim();
+            // Does this cookie string begin with the name we want?
+            if (cookie.substring(0, name.length + 1) === (name + '=')) {
+                cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
+                break;
+            }
+        }
+    }
+    return cookieValue;
+}
+const csrftoken = getCookie('csrftoken');
+
+const deleted = localStorage.getItem('title')
+if(deleted){
+    handleAlerts('danger', `"${deleted}" deleted`)
+    localStorage.clear()
+}
+
+const likeUnlikePosts = () => {
+    const likeUnlikeForms = [...document.getElementsByClassName("like-unlike-forms")]
+    likeUnlikeForms.forEach(form => form.addEventListener('submit', e =>{
+        e.preventDefault()
+        const clickedId = e.target.getAttribute('data-form-id')
+        const clickedBtn = document.getElementById(`like-unlike-${clickedId}`)
+
+        $.ajax({
+            type: 'POST',
+            url: '/like-unlike/',
+            data: {
+                'csrfmiddlewaretoken': csrftoken,
+                'pk': clickedId,
+            },
+            success: function(response){
+                clickedBtn.textContent = response.liked ? `Unlike (${response.count})`: `Like (${response.count})`
+            },
+            error: function(response){
+            }
+        })
+
+    }))
+}
+
+let visible = 3
+
+const getData = () => {
+    $.ajax({
+        type: "GET",
+        url: `/data/${visible}`,
+        success: function(response){
+            spinnerBox.classList.add('not-visible')
+
+            const data = response.data
+            setTimeout(()=>{
+                data.forEach(el => {
+
+                    postsBox.innerHTML += `
+                        <div class="card mb-2">
+                            <div class="card-body">
+                                <h5 class="card-title">${el.title}</h5>
+                                <p class="card-text">${el.body}</p>
+                            </div>
+                                <div class="card-footer">
+                                    <div class="row">
+                                        <div class="col-2">
+                                            <a href="${url}${el.id}" class="btn btn-primary">Details</a>
+                                        </div>
+                                        <div class="col-2">
+                                            <form class="like-unlike-forms" data-form-id="${el.id}">
+                                                <button class="btn btn-primary" id="like-unlike-${el.id}">${el.liked ? `Unlike (${el.count})`: `Like (${el.count})`}</button>
+                                            </form>
+                                        </div>
+                                    </div>
+                            </div>
+                        </div>
+                    `
+                });
+                likeUnlikePosts()
+            }, 100)
+            if(response.size == 0){
+                endBox.textContent = "No posts added yet..."
+            }
+
+            else if(response.size <= visible){
+                loadBtn.classList.add('not-visible')
+                endBox.textContent = "No pore posts to load..."
+            }
+        }
+    })
+}
+
+
+loadBtn.addEventListener("click", ()=>{
+    spinnerBox.classList.remove("not-visible")
+    visible += 3
+    getData()
+})
+
+let newPostId = null
+postForm.addEventListener('submit', e=>{
+    e.preventDefault()
+
+    $.ajax({
+        type: 'POST',
+        url: '',
+        data:{
+            "csrfmiddlewaretoken": csrf[0].value,
+            'title': title.value,
+            'body': body.value
+        },
+        success: function(response){
+            newPostId = response.id
+            postsBox.insertAdjacentHTML('afterbegin', `
+            <div class="card mb-2">
+                <div class="card-body">
+                    <h5 class="card-title">${response.title}</h5>
+                    <p class="card-text">${response.body}</p>
+                </div>
+                    <div class="card-footer">
+                        <div class="row">
+                            <div class="col-2">
+                                <a href="${url}${response.id}" class="btn btn-primary">Details</a>
+                            </div>
+                            <div class="col-2">
+                                <form class="like-unlike-forms" data-form-id="${response.id}">
+                                    <button class="btn btn-primary" id="like-unlike-${response.id}">Like (0)</button>
+                                </form>
+                            </div>
+                        </div>
+                </div>
+            </div>
+        `)
+        likeUnlikePosts()
+        // $('#addPostModal').modal('hide')
+        handleAlerts('success', 'New post added!')
+        // postForm.reset()
+
+        },
+        error: function(response){
+            console.log("error")
+            handleAlerts('danger', 'Oops... something went wrong')
+        },
+    })
+})
+
+addBtn.addEventListener('click', ()=>{
+    dropzone.classList.remove('not-visible')
+
+})
+
+closeBtns.forEach(btn=>btn.addEventListener('click', () =>{
+    postForm.reset()
+    if(!dropzone.classList.contains('not-visible')){
+        dropzone.classList.add('not-visible')
+    }
+    const myDropzone = Dropzone.forElement('#my-dropzone')
+    myDropzone.removeAllFiles(true)
+}))
+
+Dropzone.autoDiscover = false
+const myDropzone = new Dropzone("#my-dropzone", {
+    url: "upload/",
+    init: function(){
+        this.on('sending', function(file, xhr, formData){
+            formData.append('csrfmiddlewaretoken', csrftoken)
+            formData.append('new_post_id', newPostId)
+        })
+    },
+    maxFiles: 3,
+    maxFilesize: 4,
+    acceptedFiles: '.png, .jpg, .jpeg'
+})
+
+
+
+getData()
---
+++ src/posts/utils.py
@@ -0,0 +1,17 @@
+from .models import Post
+from profiles.models import Profile
+from django.http import HttpResponse
+from django.shortcuts import redirect
+
+def action_permission(func):
+    def wrapper(request, **kwargs):
+        pk = kwargs.get("pk")
+        post = Post.objects.get(pk=pk)
+        profile = Profile.objects.get(user=request.user)
+
+        if profile.user == post.profile.user:
+            return func(request, **kwargs)
+        else:
+            return redirect("posts:main-board")
+
+    return wrapper
